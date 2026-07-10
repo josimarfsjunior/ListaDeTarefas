5b846edad011d6f0251d71b2770b714949c646b6
DevJosimar <josimarfsjunior@gmail.com>
2026-07-10 17:53:07 -0300

    Criada a função para excluir a tarefa e após exclusão renderizar a lista de tarefas no HTML

--- src/BackEnd/script.js
+++ src/BackEnd/script.js
@@ -8,7 +8,7 @@ let buttonElement = document.querySelector("#divListaDeTarefas button");
 /**
  * Arry criado para guardar as tarefas criadas
  */
-let ListaDeTarefas = [];
+let listaDeTarefas = [];
 
 //Função para apresentar a lista de tarefas
 function renderizarTarefas() {
@@ -18,12 +18,27 @@ function renderizarTarefas() {
    * A função push() em JavaScript percorre o array original e aplica uma função de transformação em cada
    * elemento, retornando um novo array com os resultados, sem alterar a lista original.
    */
-  ListaDeTarefas.map((tarefaCriada) => {
+  listaDeTarefas.map((tarefaCriada) => {
     let liElement = document.createElement("li");
     let textoDaTarefa = document.createTextNode(tarefaCriada);
 
+    //adicionando o link de excluir no HTML
+    let linkElement = document.createElement("a");
+    linkElement.setAttribute("href", "#");
+
+    let posicaoTarefaCriada = listaDeTarefas.indexOf(tarefaCriada);
+    //Ao clicar no botão excluir chama a função de excluir a tarefa
+    linkElement.setAttribute(
+      "onclick",
+      `excluirTarefa(${posicaoTarefaCriada})`,
+    );
+
+    let textoDoLink = document.createTextNode("Excluir");
+    linkElement.appendChild(textoDoLink);
+
     //adicionando a lista no HTML
     liElement.appendChild(textoDaTarefa);
+    liElement.appendChild(linkElement);
     listElement.appendChild(liElement);
   });
 }
@@ -38,7 +53,7 @@ function adicionarTarefa() {
     let novaTarefa = inputElement.value;
 
     //A função push() em JavaScript é usada para adicionar um ou mais elementos ao final de um array.
-    ListaDeTarefas.push(novaTarefa);
+    listaDeTarefas.push(novaTarefa);
 
     inputElement.value = "";
 
@@ -47,3 +62,14 @@ function adicionarTarefa() {
 }
 
 buttonElement.onclick = adicionarTarefa;
+
+/**
+ * Função criada para excluir a tarefa
+ */
+function excluirTarefa(posicaoTarefaCriada) {
+  //método splice() do JavaScript permite adicionar, remover ou substituir elementos em um array.
+  listaDeTarefas.splice(posicaoTarefaCriada, 1);
+
+  //Após excluir a tarefa, é necessário renderizar a lista novamente no HTML
+  renderizarTarefas();
+}
